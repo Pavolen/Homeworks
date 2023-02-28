703331b9ace2051c0a21800c64695adfac557400
Olga Pavlenko <pavolen@mail.ru>
2023-02-28 22:49:12 +0300

    date

--- week13/index13week.js
+++ week13/index13week.js
@@ -5,7 +5,6 @@ const form = document.querySelector("#form");
 const chatContent = document.querySelector(".chat_content");
 const commentPhoto = document.querySelector("#comment-photo");
 const badSlang = "viagra xxx";
-const time = createElAttr("span", 'class', 'time');
 let comments = [];
 commentName.addEventListener("blur", () => {
   commentName.value = commentName.value
@@ -20,6 +19,7 @@ commentAdd.addEventListener("click", () => {
 });
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  console.log('addEventListener("submit"');
   const name = commentName.value;
   const body = commentBody.value;
   const photo = commentPhoto.value === '' ? "./img/man.png" : commentPhoto.value;
@@ -28,6 +28,7 @@ form.addEventListener("submit", (event) => {
   div.innerHTML = `
   <img class="chat_img" src="${photo}" alt="${name}" />
   <strong class="chat_name">${name}</strong>
+  <span slass="time">${returnData()}</span>
   <p class="chat_comment">${body}</p>
   `;
   chatContent.appendChild(div);
@@ -37,9 +38,6 @@ form.addEventListener("submit", (event) => {
 });
 function checkSpam(string) {
   let newString = string;
-  //  'viagra xxx'
-  //  badSlang.split(' ') == ['viagra','xxx']
-  //  item ==
   badSlang.split(" ").forEach((item) => {
     const re = new RegExp(item, "gi");
     newString = newString.replace(re, "***");
@@ -47,13 +45,14 @@ function checkSpam(string) {
   return newString;
 }
 function capitalizeFirstLetter(string) {
-  //return string.charAt(0).toUpperCase()+string.slice(1);
-  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
+function returnData(){
+
 let d = new Date();
-const day = (d.getDate( ) < 10) ? '0'+ d.getDate(): d.getDate;
+const day = (d.getDate( ) < 10) ? '0'+ d.getDate(): d.getDate();
 const month = (d.getMonth()+1 < 10) ? '0' + (d.getMonth()+1 ) : d.getMonth()+1;
-let datestring = day + "-" + month + "-" + d.getFullYear() + " " + d.getHours() + d.getMinutes();
-time.innerHTML = datestring;
+return `${day} - ${month} - ${d.getFullYear()}  ${d.getHours()}:${d.getMinutes()}`;
+}
 
 
